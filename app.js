7fe4188d3de5c646ee09cc839d13addff2392ddf
Mason Smith <smithmasons11@gmail.com>
2025-09-09 20:49:56 -0500

    Add number generator for choosing category indexs

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello jeopardy");
+function randomIndexGenerator(upperLimit, indexAmount) {
+  const numberIndex = [];
+  let randomIndex;
+  let indexAmountTracker = 0;
+  while (indexAmountTracker < indexAmount) {
+    randomIndex = Math.floor(Math.random() * upperLimit);
+    if (!numberIndex.includes(randomIndex)) {
+      numberIndex.push(randomIndex);
+      indexAmountTracker++;
+    }
+  }
+  return numberIndex;
+}
+
+async function chooseCategories(count, numOfCategories) {
+  let res = await axios.get(
+    `https://rithm-jeopardy.herokuapp.com/api/categories`,
+    { params: { count } }
+  );
+  const categoryIndex = randomIndexGenerator(count, numOfCategories);
+  console.log(categoryIndex);
+  const categoryArray = [];
+}
+
+chooseCategories(14, 6);
