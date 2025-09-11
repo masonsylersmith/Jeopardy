f9e6e848a274e9b04a98e8a1d14c5f06f4b8ccb5
Mason Smith <smithmasons11@gmail.com>
2025-09-10 20:14:07 -0500

    Develop in house category array and refine category selection

--- app.js
+++ app.js
@@ -5,30 +5,51 @@ function randomIndexGenerator(upperLimit, indexAmount) {
   let indexAmountTracker = 0;
   while (indexAmountTracker < indexAmount) {
     randomIndex = Math.floor(Math.random() * upperLimit);
-    //Tracker only increases if a new number is added to the array
+    //If statement to prevent duplicate indices
     if (!numberIndex.includes(randomIndex)) {
       numberIndex.push(randomIndex);
+      //Tracker only increases if a new number is added to the array
       indexAmountTracker++;
     }
   }
   return numberIndex;
 }
 
-//Pulls number of categories specified in parameter
-async function chooseCategories(count, numOfCategories) {
+//Generate an array with all the categories... for the purpose of deleting already chosen categories for rounds two and three
+async function generateAllCategories(count) {
+  //Pulling data from api
   let res = await axios.get(
     `https://rithm-jeopardy.herokuapp.com/api/categories`,
     { params: { count } }
   );
-  //Getting random number array
-  const categoryIndex = randomIndexGenerator(count, numOfCategories);
-  //Creates array of categories for specified number of categories
-  const categoryArray = [];
-  for (index of categoryIndex) {
-    ({ id, title } = res.data[index]);
-    categoryArray.push({ id, title });
-  }
-  console.log(categoryArray);
+  //Populating an array with the available categories
+  allCategoriesArray = [];
+  res.data.forEach((category) => {
+    ({ id, title } = category);
+    allCategoriesArray.push({ id, title });
+  });
+  //Returning an array of objects containing categories id and title
+  return allCategoriesArray;
 }
 
-chooseCategories(14, 6);
+//Pulls number of categories specified in parameter
+function chooseCategories(activeCategoriesArray, numOfCategories) {
+  chosenCategories = [];
+  //Getting random index array
+  categoryIndexList = randomIndexGenerator(
+    activeCategoriesArray.length,
+    numOfCategories
+  );
+  for (categoryIndex of categoryIndexList) {
+    chosenCategories.push(activeCategoriesArray[categoryIndex]);
+  }
+  return chosenCategories;
+}
+
+async function main() {
+  const activeCategories = await generateAllCategories(14);
+  const chosenCategories = chooseCategories(activeCategories, 6);
+  console.log(chosenCategories);
+}
+
+main();
