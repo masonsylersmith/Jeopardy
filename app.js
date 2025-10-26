1c5f4f1b97d9a722da9ce4de191ac2ce0c17ce51
Mason Smith <smithmasons11@gmail.com>
2025-10-25 20:11:10 -0500

    Add start/restart button

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ async function generateAllCategories(count) {
 
 //Pulls number of categories specified in parameter
 function chooseCategories(activeCategoriesArray, numOfCategories) {
-  chosenCategories = [];
+  let chosenCategories = [];
   //Getting random index array
-  categoryIndexList = randomIndexGenerator(
+  let categoryIndexList = randomIndexGenerator(
     activeCategoriesArray.length,
     numOfCategories
   );
   // Populating chosen categories array with random indecies and available categories
-  for (categoryIndex of categoryIndexList) {
+  for (let categoryIndex of categoryIndexList) {
     chosenCategories.push(activeCategoriesArray[categoryIndex]);
   }
   return chosenCategories;
@@ -100,62 +100,73 @@ function showPopUp(question, answer) {
   popup.style.display = "block";
 }
 
-async function main() {
+async function generateBoard() {
   //Initial category population
   let activeCategories = await generateAllCategories(14);
   //Randomly choosing 6 categories
-  let chosenCategories = chooseCategories(activeCategories, 6);
+  chosenCategories = chooseCategories(activeCategories, 6);
   //Adds clues to chosen categories array
   await addClues(chosenCategories);
   //Populates the category titles
   console.log(chosenCategories);
   populateTitles(chosenCategories);
   populatePrices();
-
-  //Selecting all the elements needing to be worked with
-  const gameBoard = document.querySelector("#gameBoardContainer");
-  const popup = document.querySelector("#popupBackdrop");
-  const seeAnswer = document.querySelector("#popupAnswerButton");
-  const popupAnswer = document.querySelector("#popupAnswer");
-  const closePopup = document.querySelector("#closePopup");
-  const categoryColumnList = document.querySelectorAll(".categoryColumn");
-  gameBoard.addEventListener("click", function (event) {
-    let clicked = event.target;
-    let parentColumn = clicked.parentElement;
-    let clueList = parentColumn.querySelectorAll(".clue");
-    let clickedCategory;
-    //Getting the column number of what was clicked
-    for (let i = 0; i < categoryColumnList.length; i++) {
-      if (categoryColumnList[i] === parentColumn) {
-        clickedCategory = i;
-      }
-    }
-    //Getting the row number of what was clicked
-    //Shows popup for that specified clue card
-    //Also removes card from future play
-    for (let i = 0; i < clueList.length; i++) {
-      if (clueList[i] === clicked && clicked.classList.contains("activeClue")) {
-        clicked.innerText = "";
-        clicked.classList.remove("activeClue");
-        showPopUp(
-          chosenCategories[clickedCategory].clues[i].question,
-          chosenCategories[clickedCategory].clues[i].answer
-        );
-      }
-    }
-    //Events for button clicks in popup
-    if (clicked === seeAnswer) {
-      seeAnswer.style.display = "none";
-      popupAnswer.style.display = "block";
-      closePopup.style.display = "block";
-    }
-    if (clicked === closePopup) {
-      seeAnswer.style.display = "block";
-      popupAnswer.style.display = "none";
-      closePopup.style.display = "none";
-      popup.style.display = "none";
-    }
-  });
 }
 
-main();
+//Set chosen categories array to be global in order to keep it persistent across different games
+let chosenCategories = [];
+//Populating the board on "start game" button press
+const startButton = document.querySelector("#gameStartButton");
+startButton.addEventListener("click", function () {
+  startButton.innerText = "Restart Game";
+  startButton.style.border = "4px solid rgb(250, 161, 63)";
+  generateBoard();
+});
+
+//Selecting all the elements needing to be worked with
+const gameBoard = document.querySelector("#gameBoardContainer");
+const popup = document.querySelector("#popupBackdrop");
+const seeAnswer = document.querySelector("#popupAnswerButton");
+const popupAnswer = document.querySelector("#popupAnswer");
+const closePopup = document.querySelector("#closePopup");
+const categoryColumnList = document.querySelectorAll(".categoryColumn");
+
+//Main gameplay event listener
+gameBoard.addEventListener("click", function (event) {
+  let clicked = event.target;
+  let parentColumn = clicked.parentElement;
+  let clueList = parentColumn.querySelectorAll(".clue");
+  let clickedCategory;
+  //Getting the column number of what was clicked
+  for (let i = 0; i < categoryColumnList.length; i++) {
+    if (categoryColumnList[i] === parentColumn) {
+      clickedCategory = i;
+    }
+  }
+  //Getting the row number of what was clicked
+  //Shows popup for that specified clue card
+  //Also removes card from future play
+  for (let i = 0; i < clueList.length; i++) {
+    if (clueList[i] === clicked && clicked.classList.contains("activeClue")) {
+      clicked.innerText = "";
+      clicked.classList.remove("activeClue");
+      showPopUp(
+        chosenCategories[clickedCategory].clues[i].question,
+        chosenCategories[clickedCategory].clues[i].answer
+      );
+      console.log(chosenCategories);
+    }
+  }
+  //Events for button clicks in popup
+  if (clicked === seeAnswer) {
+    seeAnswer.style.display = "none";
+    popupAnswer.style.display = "block";
+    closePopup.style.display = "block";
+  }
+  if (clicked === closePopup) {
+    seeAnswer.style.display = "block";
+    popupAnswer.style.display = "none";
+    closePopup.style.display = "none";
+    popup.style.display = "none";
+  }
+});
